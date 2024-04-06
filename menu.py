a44d70223610d8defe99caef33d5dfa1f094f578
Renz Angelo T. Sefuentes <142190484+220000001074@users.noreply.github.com>
2024-04-06 17:42:28 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -33,3 +33,78 @@ async def read_menu(
     raise HTTPException(status_code=404, detail="Item not found")
 
 
+# -----------------------------POST/CREATE----------------------------------
+
+@menuRouter.post("/menu/", response_model=dict)
+async def create_item(
+    item_id: int = Form(...), 
+    menuItemCategory: str = Form(...), 
+    menuItemName: str = Form(...),
+    menuItemPrice: int = Form(...),  
+    db=Depends(get_db)
+):
+
+    query = "INSERT INTO menu (ItemID, menuItemCategory, menuItemName, menuItemPrice) VALUES (%s, %s, %s, %s)"
+    db[0].execute(query, (item_id, menuItemCategory, menuItemName, menuItemPrice))
+
+    # Retrieve the last inserted ID using LAST_INSERT_ID()
+    db[0].execute("SELECT LAST_INSERT_ID()")
+    new_user_id = db[0].fetchone()[0]
+    db[1].commit()
+
+    return {"item_id": new_user_id, 
+            "menuItemCategory": menuItemCategory,
+            "menuItemName": menuItemName,
+            "menuItemPrice": menuItemPrice,     
+            }
+
+
+
+
+# ----------------PUT/UPDATE------------------------------------------------
+@menuRouter.put("/menu/{menu_id}", response_model=dict)
+async def update_order(
+    menu_id: int,
+    menuItemCategory: str,
+    menuItemName: str,
+    menuItemPrice: int,
+    db=Depends(get_db)
+):
+
+    # Update menu information in the database 
+    query = "UPDATE menu SET menuItemCategory = %s, menuItemName = %s, menuItemPrice =%s  WHERE ItemID = %s"
+    db[0].execute(query, (menuItemCategory, menuItemName, menuItemPrice, menu_id))
+
+    # Check if the update was successful
+    if db[0].rowcount > 0:
+        db[1].commit()
+        return {"message": "Item updated successfully"}
+    
+    # If no rows were affected, cashier not found
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+
+
+
+# --------------------DELETE--------------------------------
+@menuRouter.delete("/menu/{menu_id}", response_model=dict)
+async def delete_menu(
+    menu_id: int,
+    db=Depends(get_db)
+):
+    # try:
+        # Check if the item exists
+        query_check_menu = "SELECT ItemID FROM menu WHERE ItemID = %s"
+        db[0].execute(query_check_menu, (menu_id,))
+        existing_item = db[0].fetchone()
+
+        if not existing_item:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        # Delete the item
+        query_delete_item = "DELETE FROM menu WHERE ItemID = %s"
+        db[0].execute(query_delete_item, (menu_id,))
+        db[1].commit()
+
+        return {"message": "Item deleted successfully"}
