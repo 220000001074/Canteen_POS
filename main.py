d3b9ea95455bd708c0f4cdca4ecee2cb16dd2639
Renz Angelo T. Sefuentes <142190484+220000001074@users.noreply.github.com>
2024-04-06 17:46:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from model.menu import menuRouter
 from model.payment import paymentRouter
 from model.orders import ordersRouter
 from model.transaction import transactionRouter
-from model.users import UsersRouter
-from model.categories import CategoriesRouter
-from model.expenses import ExpensesRouter
+
 
 app = FastAPI()
 
@@ -17,6 +15,4 @@ app.include_router(menuRouter, prefix="/api")
 app.include_router(ordersRouter, prefix="/api")
 app.include_router(paymentRouter, prefix="/api")
 app.include_router(transactionRouter, prefix="/api")
-app.include_router(UsersRouter, prefix="/api")
-app.include_router(CategoriesRouter, prefix="/api")
-app.include_router(ExpensesRouter, prefix="/api")
+
