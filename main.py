9d1cbb39ad1b46455896ff19d1c76f879246fdf8
Renz Angelo T. Sefuentes <142190484+220000001074@users.noreply.github.com>
2024-04-08 10:54:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-# main.py
-from fastapi import FastAPI
-from model.cashier import cashierRouter
-from model.menu import menuRouter
-from model.payment import paymentRouter
-from model.orders import ordersRouter
-from model.transaction import transactionRouter
-
-
-app = FastAPI()
-
-# Include CRUD routes from modules
-app.include_router(cashierRouter, prefix="/api")
-app.include_router(menuRouter, prefix="/api")
-app.include_router(ordersRouter, prefix="/api")
-app.include_router(paymentRouter, prefix="/api")
-app.include_router(transactionRouter, prefix="/api")
 
